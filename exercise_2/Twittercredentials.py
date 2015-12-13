f6f45840657120f0baacc17cd5be44db9c8dca75
jbocharov <john@voicebase.com>
2015-12-13 14:56:43 -0800

    Populate my Twitter credentials

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "cb9gP6H7S00JvScNtkLrg1h2Y";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "INwEuKXY12XF2x1aALRMywmeHYECjGbqhz0RZRUKPmz3NCF5L0";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "2992703154-Umvt413O1IQMG70Up8QJYtJu4XikuXO2gvcGajm";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "xxbYD2KCuvQhDNMwhiOHtOX20GzDbqXCMQnsoY1DGhn8J";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
