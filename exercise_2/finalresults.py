5bf28c5295e9ed8f2d987afb36bc8eb4dc82dea2
jbocharov <john@voicebase.com>
2015-12-13 19:46:57 -0800

    Add all results mode to finalresults.py

---
+++ exercise_2/finalresults.py
@@ -0,0 +1,52 @@
+import psycopg2
+import sys
+
+def print_specific_word_count(word):
+
+  print( 'yo ' + word )
+
+
+def print_ascending_word_counts():
+  query = 'SELECT word, count FROM Tweetwordcount ORDER BY word ASC, count ASC';
+  
+  conn = get_connection()
+  cur = conn.cursor()
+  cur.execute(query)
+  records = cur.fetchall()
+  
+  for rec in records:
+    word = rec[0]
+    count = rec[1]
+    print( '(%s, %d)' % ( word, count ) )
+
+  conn.close()
+
+def print_usage():
+  print ('''
+    Usage: python finalresults.py [word]
+      if [word] is supplied, prints the count for that word
+      otherwise, prints all word counts in ascending order 
+  ''')
+
+def get_connection():
+  return psycopg2.connect(
+    database = "Tcount", 
+    user = "postgres", 
+    password = "pass", 
+    host = "localhost", 
+    port = "5432"
+  )
+
+if __name__ == '__main__':
+
+  if len(sys.argv) == 2:
+    word = sys.argv[1]
+    print_specific_word_count(word)
+
+  elif len(sys.argv) == 1:
+    print_ascending_word_counts()
+
+  else:
+    print_usage() 
+
+
